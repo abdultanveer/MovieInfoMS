3b7733c9992f104785ccc625c6f53c6adb9f98ba
abdul <abdul.tanveer@gmail.com>
2021-06-26 17:02:00 +0530

    added application name and enabled eureka client

--- MovieInfo-dnc/src/main/java/dnc/sp/ms/minfo/MovieInfoDncApplication.java
+++ MovieInfo-dnc/src/main/java/dnc/sp/ms/minfo/MovieInfoDncApplication.java
@@ -2,8 +2,10 @@ package dnc.sp.ms.minfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieInfoDncApplication {
 
 	public static void main(String[] args) {
